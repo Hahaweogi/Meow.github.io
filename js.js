5e12131f1a53ea9786697fd4566a82ec79690b76
Hahaweogi <153068580+Hahaweogi@users.noreply.github.com>
2023-12-06 16:34:20 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,32 @@
+const wrapper document.querySelector(".wrapper"); const question document.querySelector(".question");
+
+const gif document.querySelector(".gif");
+
+const yesBtn = document.querySelector(".yes-btn");
+
+const noßtn document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+
+question, InnerHTML = "Aaaaa, I like you too";
+
+gif.src =
+
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+noBtn.addEventListener("mouseover", () {
+
+const noßtnRect noßitn.getBoundingClientRect();
+
+const maxX = window.innerWidth noßtnRect.width;
+
+const maxy = window.innerHeight noBtnRect.height;
+
+const randomX = Math.floor(Math.random() maxx);
+
+const randomy = Math.floor(Math.random() maxY);
+
+noßtn.style.left randomX + "px";
+
+noßtn.style.top  randomy + "px";
+});
